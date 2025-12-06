59c0030152334dbe93191ff7282b9bb982ba956a
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2025-12-06 23:18:14 +0700

    feat: Add initial welcome page with branding, styling, and dynamic authentication links.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -218,7 +218,7 @@
         </style>
     </head>
     <body>
-        @if (Route::has('filament.app.auth.login'))
+        @if (filament()->hasLogin())
             <header class="header">
                 <nav class="nav">
                     @auth
